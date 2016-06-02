da1e8d9285e436afaf04bb256589eeb71ed9e486
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-06-02 14:44:41 +0200

    Added gzip compatible S4.Middleware

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "gzip",
     dependencies: [
     	.Package(url: "https://github.com/Zewo/zlib.git", majorVersion: 0, minor: 1),
-    	.Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 7)
+    	.Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 8),
+    	.Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 9)
     ]
 )
