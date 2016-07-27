0327b52d2f08c33eb35f94c668b63bd09b14cc89
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-07-27 09:05:59 +0200

    Updated zlib

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "gzip",
     dependencies: [
-    	.Package(url: "https://github.com/Zewo/zlib.git", majorVersion: 0, minor: 2),
+    	.Package(url: "https://github.com/Zewo/zlib.git", majorVersion: 0, minor: 3),
     	.Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 10),
     	.Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 11)
     ]
