c0c6778897f1085478ee8d3b2b31f799e100d3e7
Kenny Ackerson <kenny@tumblr.com>
2016-12-12 13:31:17 -0500

    Removing Zewo specific stuff

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "GZIP",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 14),
     	.Package(url: "https://github.com/Zewo/CZlib.git", majorVersion: 0, minor: 4),
     	.Package(url: "https://github.com/Zewo/Axis.git", majorVersion: 0, minor: 14),
 
